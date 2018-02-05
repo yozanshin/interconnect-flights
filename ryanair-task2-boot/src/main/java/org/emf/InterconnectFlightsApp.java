6320178f6007e3445631139d7b9558e7c5eb5e3a
yozanshin <yozanshin@gmail.com>
2018-02-06 00:02:17 +0100

    Removed commented code

--- ryanair-task2-boot/src/main/java/org/emf/InterconnectFlightsApp.java
+++ ryanair-task2-boot/src/main/java/org/emf/InterconnectFlightsApp.java
@@ -6,17 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//public class InterconnectFlightsApp  extends SpringBootServletInitializer{
 public class InterconnectFlightsApp{
 
     public static void main(String[] args) {
 
         SpringApplication.run(InterconnectFlightsApp.class, args);
     }
-
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//
-//        return builder.sources(InterconnectFlightsApp.class);
-//    }
 }
